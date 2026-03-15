d059db7420f54d75144d6726fea8bd9dd186c5ed
HABİBE DEMİR <dhabibe2004@gmail.com>
2026-03-16 02:39:09 +0300

    change model

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-API_URL = "https://api-inference.huggingface.co/models/dima806/food-image-classification"
+API_URL = "https://api-inference.huggingface.co/models/nateraw/food"
 
 headers = {
     "Authorization": f"Bearer {os.getenv('HUGGINGFACE_TOKEN')}"
